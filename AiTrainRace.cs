6eb1045cff435a8f6ed6319c32ca626cd9c590f1
Muhammad Talha <Talha.0801@live.com>
2022-03-29 22:34:45 +0500

    Add files via upload

---
+++ AiTrainRace.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiTrainRace : MonoBehaviour 
+{
+    public levelData LD;
+    void start()
+    {
+        LD = FindObjectOfType<levelData>();
+    }
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    if (col.CompareTag("AiTrainGo1"))
+    //    {
+    //        LD.barriertrigger[0].SetActive(false);
+    //    }
+    //    if (col.CompareTag("AiTrainGo2"))
+    //    {
+    //        LD.barriertrigger[1].SetActive(false);
+    //    }
+    //    if (col.CompareTag("AiTrainGo3"))
+    //    {
+    //        LD.barriertrigger[2].SetActive(false);
+    //    }
+    //}
+}
