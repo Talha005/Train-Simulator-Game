a4ca8323f370befb765fd60cf4fcd43c186c99fb
Muhammad Talha <Talha.0801@live.com>
2022-03-29 22:36:23 +0500

    Add files via upload

---
+++ TrainSignalRace.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainSignalRace : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject greenlight, redlight;
+	public GameObject busypanel;
+	public float Delaytime;
+
+	 void OnTriggerEnter(Collider col)
+	{
+		if (col.CompareTag("Trainplayer"))
+		{
+			Debug.Log("Trainplayer");
+			Invoke("Delay", Delaytime);
+			Invoke("Delay1", 5.0f);
+			busypanel.SetActive(true);
+			greenlight.SetActive(false);
+			redlight.SetActive(true);
+		}
+	}
+	void Delay()
+	{	
+		redlight.SetActive (false);
+		greenlight.SetActive (true);
+    }
+    void Delay1()
+	{
+		busypanel.SetActive (false);
+	}
+
+}
